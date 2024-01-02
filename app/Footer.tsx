2c37ee7378b19292cc26f5eca0cc29e32bac7c6a
kokobus <kokobus122@gmail.com>
2024-01-03 00:54:07 +0100

    Fixed footer typo

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <footer className="footer footer-center p-4 bg-base-300 text-base-content">
         <aside>
           <p>Copyright © 2024 - gdpl.vercel.app</p>
-          <p>All right reserved</p>
+          <p>All rights reserved</p>
           <p>gdpl.vercel.app is in no way affiliated with RobTopGamesAB ®</p>
         </aside>
       </footer>
